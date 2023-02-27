0e01b0f7418ca854d4d9485693240ca8252ed147
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-02-27 12:04:06 +0530

    add: save zip for exported booking data

--- includes/wp-cli-commands/class-wc-bookings-export-command.php
+++ includes/wp-cli-commands/class-wc-bookings-export-command.php
@@ -27,9 +27,33 @@ class WC_Bookings_Export_Command {
 	public function __invoke( $args, $assoc_args ) {
 		// Export all booking products.
 		if ( ! empty( $assoc_args['all'] ) ) {
+			// Default path is wp-content/uploads.
+			$directory_path = empty( $assoc_args['path'] ) ?
+				trailingslashit( WP_CONTENT_DIR ) . 'uploads' :
+				$assoc_args['path'];
+
 			try {
-				$booking_export = new WC_Bookings_Helper_Export();
-				WP_CLI::print_value( $booking_export->get_all_booking_products_data() );
+				$name_prefix   = sprintf(
+					'booking-product-%s',
+					date( 'Y-m-d',
+						current_time( 'timestamp' )
+					)
+				);
+
+				$zip_file_path = "$directory_path/$name_prefix.zip";
+				$json_file_name = "$name_prefix.json";
+
+				// Create zip;
+				$zip = new ZipArchive();
+				$zip->open( $zip_file_path, ZipArchive::CREATE );
+				$zip->addFromString(
+					$json_file_name,
+					// Get json data for all booking products.
+					( new WC_Bookings_Helper_Export() )->get_all_booking_products_data()
+				);
+				$zip->close();
+
+				WP_CLI::success( "Booking products exported. Location: $zip_file_path" );
 			} catch ( Exception $e ) {
 				WP_CLI::error( $e->getMessage() );
 			}
